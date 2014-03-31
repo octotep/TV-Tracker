a526f1af4e2b5787431bd77e77cdcee74125cfe9
Austin F <aforry2@ycp.edu>
2014-03-31 13:46:00 -0400

    Something

--- TV-Tracker-Persistence/src/com/goddammitus/tvtracker/server/model/persist/FakeDatabase.java
+++ TV-Tracker-Persistence/src/com/goddammitus/tvtracker/server/model/persist/FakeDatabase.java
@@ -7,10 +7,12 @@ import com.tvtracker.model.Account;
 
 public class FakeDatabase implements IDatabase {
 	private List<Account> accountList;
+	private Account dummyAccount(Austin, 12345);
 	
 	public FakeDatabase() {
 		this.accountList = new ArrayList<Account>();
 		// TODO: add sample accounts
+		accountList.add(new Account());
 	}
 
 	@Override
