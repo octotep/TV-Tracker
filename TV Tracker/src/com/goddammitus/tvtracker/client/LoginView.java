42a50395aa34902113d7aaa5a95fb50cfadd8866
Austin Forry <aforry2@ycp.edu>
2014-04-03 23:00:42 -0400

    Created a LoginView.  Implementing IsWidget should allow for GWT use, however I get errors suggesting I'm missing parts of GWT packages, may be an error on my end with my GWT installation.
    Someone try to attempt to edit using GWT and tell me if it's just a
    problem on my end.

---
+++ TV Tracker/src/com/goddammitus/tvtracker/client/LoginView.java
@@ -0,0 +1,13 @@
+package com.goddammitus.tvtracker.client;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+public class LoginView implements IsWidget {
+
+	@Override
+	public Widget asWidget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//TODO
+}
