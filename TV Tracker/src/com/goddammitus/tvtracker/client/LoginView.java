e092c27ec4ad87450fa93533efc74f3947afb8d7
Chris Yealy <cyealy@ycp.edu>
2014-04-04 00:25:14 -0400

    Added gwt config files to resolve dependeicies

--- TV Tracker/src/com/goddammitus/tvtracker/client/LoginView.java
+++ TV Tracker/src/com/goddammitus/tvtracker/client/LoginView.java
@@ -2,16 +2,12 @@ package com.goddammitus.tvtracker.client;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.tvtracker.model.Account;
-import com.google.gwt.user.client.ui.TextBox;
 
 public class LoginView extends Composite implements IsWidget {
 	private Account model;
 
 	public LoginView() {
-		
-		TextBox textBox = new TextBox();
-		initWidget(textBox);
-		
+
 	}
 
 	public void setModel(Account model) {
