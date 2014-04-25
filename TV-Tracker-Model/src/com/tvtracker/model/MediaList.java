fd1a772b1047eaed25485326c79ca90434ce862b
Austin F <aforry2@ycp.edu>
2014-04-25 13:24:49 -0400

    added comments to classes to better help understand interconnnectivity.

--- TV-Tracker-Model/src/com/tvtracker/model/MediaList.java
+++ TV-Tracker-Model/src/com/tvtracker/model/MediaList.java
@@ -9,23 +9,38 @@ import java.util.ArrayList;
  *
  */
 
+/*
+ * constructor
+ */
 public class MediaList implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private ArrayList<Media> list;
-
+	
+	/*
+	 * initializes an ArrayList of Media objects, i.e. the MediaList
+	 */
 	public MediaList() {
 		this.list = new ArrayList<Media>();
 	}
-
+	
+	/*
+	 * add a new Media object to the current MediaList (ArrayList<Media>)
+	 */
 	public void addMedia(Media media) {
 		this.list.add(media);
 	}
 	
+	/*
+	 * returns the MediaList
+	 */
 	public MediaList getMediaList() {
 		return this;
 	}
 	
+	/*
+	 * get Media object from index of MediaList at "number"
+	 */
 	public Media getMedia(int number) {
 		return this.list.get(number);
 	}
--- TV-Tracker-Model/src/com/tvtracker/model/MediaListProgress.java
+++ TV-Tracker-Model/src/com/tvtracker/model/MediaListProgress.java
@@ -9,39 +9,65 @@ import java.util.HashMap;
  *
  */
 
+/*
+ * constructor
+ */
 public class MediaListProgress implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private MediaProgress mediaProgress;
 	private MediaList mediaList;
-
+	
+	/*
+	 * initializes mediaList and mediaProgress
+	 */
 	public MediaListProgress() {
 		this.mediaList = new MediaList();
+		//MediaProgress takes a Media and Progress object and combines them
+		//into a HashMap together, so Media can be tied to Progress
 		this.mediaProgress = new MediaProgress(new HashMap<Media, Progress>());
 	}
 	
+	/*
+	 * sets MediaListProgress taking in a MediaList and MediaProgress object
+	 */
 	public MediaListProgress(MediaList mediaList, MediaProgress mediaProgress) {
 		this.mediaList = mediaList;
 		this.mediaProgress = mediaProgress;
 	}
-
+	
+	/*
+	 * adds media to the MediaList then adds media and creates a new Progress object for it
+	 */
 	public void addMedia(Media media, int episodesSeenInCurrentSeason, int currentSeason) {
 		mediaList.addMedia(media);
 		mediaProgress.addMedia(media, new Progress(episodesSeenInCurrentSeason, currentSeason));
 	}
-
+	
+	/*
+	 * return current mediaProgress
+	 */
 	public Progress currentProgress(Media media) {
 		return mediaProgress.getProgress(media);
 	}
 	
+	/*
+	 * return current media at index "number"
+	 */
 	public Media getMedia(int number) {
 		return this.mediaList.getMedia(number);
 	}
 	
+	/*
+	 * set current mediaList
+	 */
 	public void setMediaList(MediaList mediaList) {
 		this.mediaList = mediaList;
 	}
 	
+	/*
+	 * set current mediaProgress
+	 */
 	public void setMediaProgress(MediaProgress mediaProgress) {
 		this.mediaProgress = mediaProgress;
 	}
