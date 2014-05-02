5549a1eb20ed0d5eca90ad0f8eec3f5c27056164
Austin Decker <adecker5@ycp.edu>
2014-05-02 13:51:47 -0400

    Ignore this - have to commit to pull Dan's changes

--- TV Tracker/src/com/goddammitus/tvtracker/server/LoginServiceImpl.java
+++ TV Tracker/src/com/goddammitus/tvtracker/server/LoginServiceImpl.java
@@ -23,7 +23,7 @@ public class LoginServiceImpl extends RemoteServiceServlet implements LoginServi
 	public boolean checkIfAccountExists(String username) {
 		LoginController controller = new LoginController();
 		
-		System.out.println("Success");
+		System.out.println("Account " + (controller.checkIfAccountExists(username) ? "found" : "not found"));
 		
 		return controller.checkIfAccountExists(username);
 	}
